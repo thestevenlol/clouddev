cdde23ebd8ef88dc3e59ca0401b769df2f94bce7
thestevenlol <jackfdevelopment@gmail.com>
2023-11-06 15:11:26 +0000

    Created index.html template.

--- webapp/app.py
+++ webapp/app.py
@@ -3,15 +3,35 @@ from flask import Flask, render_template  # from module import Class.
 
 import hfpy_utils
 import swim_utils
+import os
 
 
 app = Flask(__name__)
 
+"""
+Requirements:
+- Display an ordered list of swimmers in a select list.
+- When a swimmer is selected, display their events.
+- When an event is selected, open their chart in a new tab.
+- Once tested, deploy on PythonAnywhere, configure account settings to set teacher to barryp.
+"""
+
 
 @app.get("/")
-@app.get("/hello")
-def hello():
-    return "Hello from my first web app - cool, isn't it?"  # ANY string.
+def index():
+    files = os.listdir("swimdata")
+    files.remove(".DS_Store")
+    swimmers = []
+    for f in files:
+        f.removesuffix(".txt")
+        split = f.split("-")
+        name = split[0]
+        if name not in swimmers:
+            swimmers.append(name)
+
+        swimmers.sort()
+
+    return render_template("index.html", title="Swimmer Charts", swimmers=swimmers)
 
 
 @app.get("/chart")
@@ -28,7 +48,7 @@ def display_chart():
 
     the_title = f"{name} (Under {age}) {distance} {stroke}"
     from_max = max(converts) + 50
-    the_converts = [ hfpy_utils.convert2range(n, 0, from_max, 0, 350) for n in converts ]
+    the_converts = [hfpy_utils.convert2range(n, 0, from_max, 0, 350) for n in converts]
 
     the_data = zip(the_converts, the_times)
 
