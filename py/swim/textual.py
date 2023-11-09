ae5a41807586640c0ffdfd4b4d8185d62d98d59f
thestevenlol <jackfdevelopment@gmail.com>
2023-11-09 16:49:49 +0000

    Conflict

--- py/swim/textual.py
+++ py/swim/textual.py
@@ -1,3 +1,22 @@
-from rich import print
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer
 
-print("Hello, [bold magenta]World[/bold magenta]!", ":thumbs_up:")
+
+class StopwatchApp(App):
+    """A Textual app to manage stopwatches."""
+
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield Header()
+        yield Footer()
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.dark = not self.dark
+
+
+if __name__ == "__main__":
+    app = StopwatchApp()
+    app.run()
